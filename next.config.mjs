5787ba7d96a1970a0295fde3e17d0e0ed047fb59
karim <sheriefkarim@gmail.com>
2024-03-29 01:47:34 +0200

    test deploy

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
     images: {
         domains: ['res.cloudinary.com'],
         formats: ['image/avif', 'image/webp']
-    }
-
+    },
+    distDir: 'dist'
 };
 
 export default nextConfig;
